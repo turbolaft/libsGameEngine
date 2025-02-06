31aa03ba82f9be5b4d1419ea08b15ec97451082a
Artem <turbolaft@gmail.com>
2025-02-06 20:49:13 +0100

    first commit

---
+++ resources/Models/Rectangle.h
@@ -0,0 +1,7 @@
+const float rectangle[] = {
+    // Positions          // Normals           // Texture Coords
+    -0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    0.0f, 0.0f, // Bottom-left
+     0.5f, -0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    1.0f, 0.0f, // Bottom-right
+     0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    1.0f, 1.0f, // Top-right
+    -0.5f,  0.5f, 0.0f,    0.0f, 0.0f, 1.0f,    0.0f, 1.0f  // Top-left
+};
